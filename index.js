f4b109cb3cff39560cc087d46a64451d02cecfac
Jiwa <48976957+jiwa23@users.noreply.github.com>
2022-02-19 05:06:46 +0000

    make it better

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const cors = require("cors");
 require("dotenv").config();
 // const cookieParser = require("cookie-parser");
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 // use  "npm devStart" for you local host
 
 app.use(express.json()); //method inbuilt in express() to recognize the incoming Request Object as a JSON Object
-app.use(cors());
+app.use(cors(corsOptions));
 // app.use(cookieParser);
 
 //Router
 const postRouter = require("./routes/Posts");
-app.use("/posts", postRouter); // so it becomes https://cors-anywhere.herokuapp.com/https://post-website-server.herokuapp.com/posts
+app.use("/posts", postRouter); // so it becomes https://post-website-server.herokuapp.com/posts
 
 const commentsRouter = require("./routes/Comments");
-app.use("/comments", commentsRouter); // so it becomes https://cors-anywhere.herokuapp.com/https://post-website-server.herokuapp.com/comments
+app.use("/comments", commentsRouter); // so it becomes https://post-website-server.herokuapp.com/comments
 
 const usersRouter = require("./routes/Users");
-app.use("/auth", usersRouter); // so it becomes https://cors-anywhere.herokuapp.com/https://post-website-server.herokuapp.com/auth
+app.use("/auth", usersRouter); // so it becomes https://post-website-server.herokuapp.com/auth
 
 const likesRouter = require("./routes/Likes");
-app.use("/likes", likesRouter); // so it becomes https://cors-anywhere.herokuapp.com/https://post-website-server.herokuapp.com/auth
+app.use("/likes", likesRouter); // so it becomes https://post-website-server.herokuapp.com/auth
 
 // get the database models
 const db = require("./models");
